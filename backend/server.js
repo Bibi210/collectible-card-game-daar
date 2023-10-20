5ff054845551e522d9a57ee03972d2f4abf2af44
samarlahmar24 <samarlahmar24@gmail.com>
2023-10-20 13:53:30 +0200

    affichage de carte + page home

--- backend/server.js
+++ backend/server.js
@@ -1,29 +1,43 @@
 const express = require('express');
+const axios = require('axios');
+const fs = require('fs');
+
+
 const app = express();
 const port = process.env.PORT || 3000; 
-
+const searchQuery = 'set.id:sv1'; // Replace with your desired search query
+const page = 1;
+const pageSize = 250;
+const orderBy = '-set.releaseDate'; // Replace with your desired ordering field
+const selectFields = 'id,name,images';
 
 app.use(express.json());
 
-const nftInfo = {
-  cardName: "My NFT Card",
-  illustration: "https://example.com/illustration.jpg",
-};
-
-const nftInfo2 = {
-    cardName: "My NFT Card2",
-    illustration: "https://example.com/illustration.jpg",
-  };
-
-app.get('/nft', (req, res) => {
-  res.json(nftInfo);
+axios({
+  method: 'get',
+  url: 'https://api.pokemontcg.io/v2/cards',
+  Authorization: '03afe08b-77c3-42b8-886d-638a60b66f37',
+  params : {
+    q: searchQuery,
+    page: page,
+    pageSize: pageSize,
+    orderBy: orderBy,
+    select: selectFields
+  }
   
-});
+ 
+})
 
-app.get('/nft2', (req, res) => {
-    res.json(nftInfo2);
-    
-  });
+  .then((response) => {
+    // en cas de réussite de la requête
+    const jsonData = response.data.data;
+    const jsonString = JSON.stringify(jsonData, null, 2); // Indentation de 2 espaces pour une meilleure lisibilité
+
+    // Enregistrez le JSON dans un fichier
+    fs.writeFileSync('pokemon_data.json', jsonString);
+    console.log('Données enregistrées dans pokemon_data.json');
+
+  })
 
 
 // Start the server
