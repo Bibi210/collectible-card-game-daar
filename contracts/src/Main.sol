d407a98ecf0f895f58beb2c712bc10bf5416e2a6
Bibi210 <brahimadibassi@gmail.com>
2023-10-22 19:34:44 +0200

    Save

--- contracts/src/Main.sol
+++ contracts/src/Main.sol
@@ -2,16 +2,65 @@
 pragma solidity ^0.8;
 
 import "./Collection.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract Main {
-  int private count;
-  mapping(int => Collection) private collections;
+contract Main is Ownable {
+  uint public nbCollections;
+  mapping(uint => Collection) private _collections;
 
-  constructor() {
-    count = 0;
+  constructor(address initialOwner) Ownable(initialOwner) {
+    nbCollections = 0;
   }
 
-  function createCollection(string calldata name, int cardCount) external {
-    collections[count++] = new Collection(name, cardCount);
+  function createCard(
+    uint collection_id,
+    string calldata uri
+  ) external onlyOwner {
+    Collection collection = _collections[collection_id];
+    collection.safeMint(this.owner(), uri);
+  }
+
+  function getCollection(
+    uint collection_id
+  ) external view returns (Collection) {
+    return _collections[collection_id];
+  }
+
+  function createCollection(
+    string calldata name,
+    string calldata symbol,
+    uint nbTokens
+  ) external onlyOwner {
+    Collection collection = new Collection(
+      this.owner(),
+      name,
+      symbol,
+      nbTokens
+    );
+    _collections[nbCollections] = collection;
+    nbCollections++;
+  }
+
+  function assignCard(
+    uint collection_id,
+    uint token_id,
+    address to
+  ) external onlyOwner {
+    Collection collection = _collections[collection_id];
+    collection.transferFrom(this.owner(), to, token_id);
+  }
+
+  function assignN_Cards(
+    uint collection_id,
+    uint n,
+    address to
+  ) external onlyOwner {
+    Collection collection = _collections[collection_id];
+    uint nbSysOwned = collection.balanceOf(this.owner());
+    if (n > nbSysOwned) revert("Main: not enough cards");
+    for (uint i = 0; i < n; i++) {
+      uint tokenId = collection.tokenOfOwnerByIndex(this.owner(), i);
+      collection.transferFrom(this.owner(), to, tokenId);
+    }
   }
 }
