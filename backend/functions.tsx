699ff1a3eea6c54935083d90de18e2119ae04628
samarlahmar24 <samarlahmar24@gmail.com>
2023-10-23 21:12:55 +0200

    ajout connexion metamask

---
+++ backend/functions.tsx
@@ -0,0 +1,8 @@
+
+
+
+/** une fonction qui prend la clé privée d'un utilisateur et renvoie la liste d'id des cartes qu'il possede */
+function  getUserCards( privateKey : string) : string[] {
+    let idCard =[]
+    return idCard ;
+}
