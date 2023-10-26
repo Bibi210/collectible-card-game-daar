f46e17295d1a514d23459b9f73b0b65908147ff8
samarlahmar24 <samarlahmar24@gmail.com>
2023-10-26 14:28:32 +0200

    add profile

--- frontend/functions.js
+++ frontend/functions.js
@@ -1,8 +1,6 @@
 
-
-
 /** une fonction qui prend la clé privée d'un utilisateur et renvoie la liste d'id des cartes qu'il possede */
-function  getUserCards( privateKey : string) : string[] {
-    let idCard =[]
+function getUserCards( privateKey ) {
+    let idCard =["sk","sd"]
     return idCard ;
 }
--- frontend/index.js
+++ frontend/index.js
@@ -1,36 +1,40 @@
+var currentAccount = null;
+
+
+
 // You should only attempt to request the user's account in response to user
 // interaction, such as selecting a button.
 // Otherwise, you popup-spam the user like it's 1999.
 // If you fail to retrieve the user's account, you should encourage the user
 // to initiate the attempt.
+
 document.addEventListener('DOMContentLoaded', function () {
-const ethereumButton = document.querySelector('.enableEthereumButton');
-const showAccount = document.querySelector('.showAccount');
-
-
-ethereumButton.addEventListener('click', () => {
-  getAccount();
-});
-
-// While awaiting the call to eth_requestAccounts, you should disable
-// any buttons the user can select to initiate the request.
-// MetaMask rejects any additional requests while the first is still
-// pending.
-async function getAccount() {
-  const accounts = await window.ethereum.request({ method: 'eth_requestAccounts' })
-    .catch((err) => {
-      if (err.code === 4001) {
-        // EIP-1193 userRejectedRequest error
-        // If this happens, the user rejected the connection request.
-        console.log('Please connect to MetaMask.');
-      } else {
-        console.error(err);
-      }
-    });
-  const account = accounts[0];
-  showAccount.innerHTML = account;
-}
-let currentAccount = null;
+  const ethereumButton = document.querySelector('.enableEthereumButton');
+  const showAccount = document.querySelector('.showAccount');
+ 
+  ethereumButton.addEventListener('click', () => {
+    getAccount();
+  });
+  
+  // While awaiting the call to eth_requestAccounts, you should disable
+  // any buttons the user can select to initiate the request.
+  // MetaMask rejects any additional requests while the first is still
+  // pending.
+  async function getAccount() {
+    const accounts = await window.ethereum.request({ method: 'eth_requestAccounts' })
+      .catch((err) => {
+        if (err.code === 4001) {
+          // EIP-1193 userRejectedRequest error
+          // If this happens, the user rejected the connection request.
+          console.log('Please connect to MetaMask.');
+        } else {
+          console.error(err);
+        }
+      });
+    const account = accounts[0];
+    showAccount.innerHTML = account;
+  }
+  
 window.ethereum.request({ method: 'eth_accounts' })
   .then(handleAccountsChanged)
   .catch((err) => {
